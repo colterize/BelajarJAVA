710c1cf2328b718b8dffa8236507064329944719
yaniyani <yani@ist.id>
2025-03-06 09:46:56 +0700

    simplify Instrument using Lombok and implement log

--- src/main/java/com/example/helloWorld/controller/InstrumentController.java
+++ src/main/java/com/example/helloWorld/controller/InstrumentController.java
@@ -3,6 +3,7 @@ package com.example.helloWorld.controller;
 import com.example.helloWorld.dto.Instrument;
 import com.example.helloWorld.repository.InstrumentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/instruments")
 @RequiredArgsConstructor
@@ -29,11 +31,13 @@ public class InstrumentController {
 
     @PostMapping("/find")
     public Optional<Instrument> findInstrument(@RequestBody Instrument instrument) {
+        log.info("Request Find: {}", instrument.getId());
         return instrumentRepository.findById(instrument.getId());
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteInstrument(@RequestBody Instrument instrument) {
+        log.info("Request Delete: {}", instrument.getId());
         if (instrumentRepository.existsById(instrument.getId())) {
             instrumentRepository.deleteById(instrument.getId());
             return ResponseEntity.ok("Instrument deleted successfully");
--- src/main/java/com/example/helloWorld/dto/Instrument.java
+++ src/main/java/com/example/helloWorld/dto/Instrument.java
@@ -1,35 +1,19 @@
 package com.example.helloWorld.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "instruments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Instrument {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String brand;
-
-    public Instrument() {}
-
-    public Instrument(String brand) {
-        this.brand = brand;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
 }
